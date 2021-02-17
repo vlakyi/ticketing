6eb181f4cb3f213d2c3b13bceec53dc58f89d922
Vlad <kiyashko.vlad1@gmail.com>
2021-02-18 00:20:23 +0100

    importing new rotes for signing, signout, signup and currentUser

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,19 @@
 import express from 'express';
-import { json } from 'body-parser';
+
+// Importing routes
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
 
 const app = express();
-app.use(json());
+app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi there!');
-});
+// Add routes
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signoutRouter);
+app.use(signupRouter);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
