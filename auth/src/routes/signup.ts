0bd7449d69be80be6bb234a402695e8efaa2f462
Vlad <kiyashko.vlad1@gmail.com>
2021-02-18 02:34:08 +0100

    adding custom errors for testing purposes

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,7 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import { RequestValidationError } from '../errors/request-validation-error';
+import { DatabaseConnectionError } from '../errors/database-connection-error';
 
 const router = express.Router();
 
@@ -7,22 +9,20 @@ router.post('/api/users/signup', [
   body('email')
     .isEmail()
     .withMessage('Email must be valid'),
-  body('passord')
+  body('password')
     .trim()
     .isLength({ min: 4, max: 20 })
     .withMessage('Password must be between 4 and 20 characters')
 ],
   (req: Request, res: Response) => {
     const errors = validationResult(req);
-
+    console.log(errors);
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
 
-    const { email, password } = req.body;
-
     console.log('Creating a user...');
-
+    throw new DatabaseConnectionError();
     res.send({});
   });
 
