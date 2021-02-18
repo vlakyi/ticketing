9961f49747ab9bfa272a17e6e815f961c74ce35c
Vlad <kiyashko.vlad1@gmail.com>
2021-02-18 02:34:52 +0100

    adding errorHandler middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import 'express-async-errors';
 
-// Importing routes
+// Routes
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
+
+// Middlewares
+import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
 app.use(express.json());
@@ -14,6 +18,9 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+// Add middlewares
+app.use(errorHandler);
 
 app.listen(3000, () => {
   console.log('Listening on port 3000!');
