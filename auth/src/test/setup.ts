1b7e5c425f2805acff812f9760899622759b06da
Vlad <kiyashko.vlad1@gmail.com>
2021-02-22 01:43:03 +0100

    adding new global scope function for test reusability

--- auth/src/test/setup.ts
+++ auth/src/test/setup.ts
@@ -1,9 +1,20 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
 import { app } from '../app';
+import request from 'supertest';
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signin (): Promise<string[]>;
+    }
+  }
+}
 
 let mongo: any;
 beforeAll(async () => {
+  // should be changed
+  process.env.JWT_KEY = 'asdfasdf';
   mongo = new MongoMemoryServer();
   const mongoUri = await mongo.getUri();
 
@@ -25,3 +36,19 @@ beforeEach(async () => {
 afterAll(async () => {
   await mongo.stop();
 });
+
+//  Creating global scope function
+
+global.signin = async () => {
+  const email = 'test@test.com';
+  const password = 'password';
+
+  const response = await request(app)
+    .post('/api/users/signup')
+    .send({
+      email, password
+    })
+    .expect(201);
+
+  return response.get('Set-Cookie');
+};
