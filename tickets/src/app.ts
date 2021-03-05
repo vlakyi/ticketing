bb236c8380f0a91d497103afe7cfa96a55d72c28
Vlad <kiyashko.vlad1@gmail.com>
2021-03-05 01:30:12 +0100

    adding routes and middlewares to app.ts

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -1,8 +1,14 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 
+// Routes
+import { createTicketRouter } from './routes/new';
+import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
+import { updateTicketRouter } from './routes/update';
+
 // Middlewares
-import { errorHandler } from '@vlakyi-org/common';
+import { errorHandler, currentUser } from '@vlakyi-org/common';
 import cookieSession from 'cookie-session';
 
 const app = express();
@@ -14,5 +20,13 @@ app.use(cookieSession({
   signed: false,
   secure: process.env.NODE_ENV !== 'test'
 }));
+
+app.use(currentUser);
+
+// Routes
+app.use(createTicketRouter);
+app.use(showTicketRouter);
+app.use(indexTicketRouter);
+app.use(updateTicketRouter);
 
 export { app };
