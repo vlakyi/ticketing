8d77c2d28d9450fb77edcfd931dd29a5eca9d973
Vlad <kiyashko.vlad1@gmail.com>
2021-03-05 01:25:04 +0100

    setting up testing environment and creating testing utility file

--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,12 +1,12 @@
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import mongoose from 'mongoose';
-import { app } from '../app';
-import request from 'supertest';
+import jwt from 'jsonwebtoken';
+import { generateMongooseObjID } from './utils';
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin (): Promise<string[]>;
+      signin (): string[];
     }
   }
 }
@@ -39,16 +39,25 @@ afterAll(async () => {
 
 //  Creating global scope function
 
-global.signin = async () => {
-  const email = 'test@test.com';
-  const password = 'password';
+global.signin = () => {
+  // Build a JWT  payload. {id, email}
+  const payload = {
+    id: generateMongooseObjID(),
+    email: 'test@test.com'
+  };
 
-  const response = await request(app)
-    .post('/api/users/signup')
-    .send({
-      email, password
-    })
-    .expect(201);
+  // Create the JWT!
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
-  return response.get('Set-Cookie');
+  // Build session Object. { jwt: MY_JWT }
+  const session = { jwt: token };
+
+  // Turn that session info JSON
+  const sessionJSON = JSON.stringify(session);
+
+  // Take JSON and encode it as base64
+  const base64 = Buffer.from(sessionJSON).toString('base64');
+
+  // return a string thats the cookie with the encoded data
+  return [ `express:sess=${base64}` ];
 };
