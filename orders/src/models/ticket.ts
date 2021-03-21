6259e7d90337b966b08ec6c811044177317168ec
Vlad <kiyashko.vlad1@gmail.com>
2021-03-21 21:07:26 +0100

    adding id parameter inside TicketAttrs interface, changing ticketSchema build method implementation to fill _id field based on TicketAttrs["id"]

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -2,6 +2,7 @@ import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
 
 interface TicketAttrs {
+  id: string;
   title: string;
   price: number;
 }
@@ -36,7 +37,12 @@ const ticketSchema = new mongoose.Schema({
 });
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
-  return new Ticket(attrs);
+  const { id, title, price } = attrs;
+  return new Ticket({
+    _id: id,
+    title,
+    price
+  });
 };
 
 ticketSchema.methods.isReserved = async function () {
